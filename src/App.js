c62ce39a7377b5c112cd6e67b13e9137479c22a2
dvbaek <105186138+dvbaek@users.noreply.github.com>
2023-11-08 12:25:00 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import "./App.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import MainPage from "./pages/MainPage";
 import JoinPage from "./pages/JoinPage";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App"> 
-      <JoinPage/>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage/>}></Route>
+          <Route path="/join" element={<JoinPage/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
