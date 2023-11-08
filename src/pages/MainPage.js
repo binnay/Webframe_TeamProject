f01e0f7256d1b5d0c0a4ad464f93af989042afed
dvbaek <105186138+dvbaek@users.noreply.github.com>
2023-11-08 12:24:38 +0900

    Update MainPage.js

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,6 +2,7 @@ import Carousel from "../components/Carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import TopMenuBar from "../components/TopMenuBar";
 import './MainPage.css';
+import { Link } from 'react-router-dom';
 
 function MainPage() {
     return (
@@ -9,7 +10,7 @@ function MainPage() {
             <TopMenuBar/>
             <Carousel />
             <button type="button" id="intro2">동아리 소개 </button>
-            <button type="button" id="join2"> 동아리 지원 </button>
+            <Link to="/join"><button type="button" id="join2"> 동아리 지원 </button></Link>
         </div>
 
     );
