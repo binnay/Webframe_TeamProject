d0750872eca323abaa4df18f83bc712376954f77
dvbaek <105186138+dvbaek@users.noreply.github.com>
2023-11-08 10:34:20 +0900

    Create JoinBox.js

---
+++ src/components/JoinBox.js
@@ -0,0 +1,14 @@
+import './JoinBox.css';
+import React from 'react';
+
+function JoinBox({crewName, crewImg, join_date}) {
+    return (
+        <div className="crew-container">
+            <img className="crewImg" src={crewImg} alt="동아리사진"/>
+            <div className="crewName">{crewName}</div>
+            <div className="joinDate">{join_date}</div>
+        </div>
+    )
+}
+
+export default JoinBox;
