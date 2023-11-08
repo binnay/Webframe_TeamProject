2c7937c0b1daf759d71d6d80af6c6a7ff4aa0966
dvbaek <105186138+dvbaek@users.noreply.github.com>
2023-11-08 10:40:31 +0900

    Create dataDummy.js

---
+++ src/dataDummy.js
@@ -0,0 +1,88 @@
+import wangSanAk from './media/왕산악.png';
+import deulBul from './media/들불.png';
+import artCrew from './media/민족예술학회.png';
+import doIt from './media/DoIt.png';
+import meetingNom from './media/미팅놈들.png';
+import teddyBear from './media/곰돌이.png';
+import cat from './media/고양이.png';
+
+export const dataDummy = [
+    {
+        filter: "교내동아리",
+        crewName: "왕산악",
+        crewImg: wangSanAk,
+        join_date: "12월 5일"
+    },
+    {
+        filter: "교내동아리",
+        crewName: "들불",
+        crewImg: deulBul,
+        join_date: "12월 6일"
+    },
+    {
+        filter: "교내동아리",
+        crewName: "민족예술학회",
+        crewImg: artCrew,
+        join_date: "12월 7일"
+    },
+    {
+        filter: "교내동아리",
+        crewName: "왕산악",
+        crewImg: wangSanAk,
+        join_date: "12월 5일"
+    },
+    {
+        filter: "교내동아리",
+        crewName: "들불",
+        crewImg: deulBul,
+        join_date: "12월 6일"
+    },
+    {
+        filter: "연합동아리",
+        crewName: "DoIt",
+        crewImg: doIt,
+        join_date: "11월 5일"
+    },
+    {
+        filter: "연합동아리",
+        crewName: "미팅놈들",
+        crewImg: meetingNom,
+        join_date: "11월 30일"
+    },
+    {
+        filter: "연합동아리",
+        crewName: "DoIt",
+        crewImg: doIt,
+        join_date: "11월 5일"
+    },
+    {
+        filter: "연합동아리",
+        crewName: "미팅놈들",
+        crewImg: meetingNom,
+        join_date: "11월 30일"
+    },
+    {
+        filter: "소모임",
+        crewName: "곰돌이",
+        crewImg: teddyBear,
+        join_date: "11월 30일"
+    },
+    {
+        filter: "소모임",
+        crewName: "고양이",
+        crewImg: cat,
+        join_date: "11월 2일"
+    },
+    {
+        filter: "소모임",
+        crewName: "곰돌이",
+        crewImg: teddyBear,
+        join_date: "11월 30일"
+    },
+    {
+        filter: "소모임",
+        crewName: "고양이",
+        crewImg: cat,
+        join_date: "11월 2일"
+    }
+]
