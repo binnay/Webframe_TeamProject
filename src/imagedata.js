ff6fb7ab3b881e6070e5dec64174ac3b3d6e6809
dvbaek <105186138+dvbaek@users.noreply.github.com>
2023-11-08 10:40:04 +0900

    Create imagedata.js

---
+++ src/imagedata.js
@@ -0,0 +1,16 @@
+// imagedata.js
+import slideImage1 from './media/slideImage1.png';
+import slideImage2 from './media/slideImage2.jpg';
+
+const imageData = [
+    {
+        label: "slideImg1",
+        src: slideImage1,
+    },
+    {
+        label: "slideImg2",
+        src: slideImage2,
+    },
+];
+
+export default imageData;
