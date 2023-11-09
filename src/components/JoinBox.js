06526a613fe75bb9f889a4eabfee9e47fa7a9db1
dvbaek <105186138+dvbaek@users.noreply.github.com>
2023-11-09 12:10:02 +0900

    Update JoinBox.js

--- src/components/JoinBox.js
+++ src/components/JoinBox.js
@@ -6,7 +6,9 @@ function JoinBox({crewName, crewImg, join_date}) {
         <div className="crew-container">
             <img className="crewImg" src={crewImg} alt="동아리사진"/>
             <div className="crewName">{crewName}</div>
-            <div className="joinDate">{join_date}</div>
+            <div className="joinDate">
+                지원기간: {join_date}
+            </div>
         </div>
     )
 }
