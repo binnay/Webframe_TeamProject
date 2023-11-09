e36e5d48cafe94a32904c4c8b627b6c8cfe8f5c4
dvbaek <105186138+dvbaek@users.noreply.github.com>
2023-11-09 12:09:26 +0900

    Create CheckBoxList.js

---
+++ src/components/CheckBoxList.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import './CheckBoxList.css';
+
+function CheckBoxList() {
+    const [isChecked, setIsChecked] = useState(false);
+
+    const toggleCheckbox = () => {
+        setIsChecked(!isChecked);
+    };
+
+    return (
+        <div className="checkBoxDiv">
+            <div>
+                <label className="switch">
+                    <input
+                        type="checkbox"
+                        id="모집현황"
+                        checked={isChecked}
+                        onChange={toggleCheckbox}
+                    />
+                    <span className="slider"></span>
+                </label>
+                <label>
+                    <span className="statusText">
+                        {isChecked ? '모집중' : '전체'}
+                    </span>
+                </label>
+            </div>
+        </div>
+    );
+}
+
+export default CheckBoxList;
