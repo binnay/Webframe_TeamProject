da1df96b69c5771346d2da5dc9ba5c580e346ee1
dvbaek <105186138+dvbaek@users.noreply.github.com>
2023-11-09 12:15:38 +0900

    Update dataDummy.js

--- src/dataDummy.js
+++ src/dataDummy.js
@@ -11,78 +11,91 @@ export const dataDummy = [
         filter: "교내동아리",
         crewName: "왕산악",
         crewImg: wangSanAk,
-        join_date: "12월 5일"
+        join_date: "11월 21일 ~ 12월 5일",
+        joining: false
     },
     {
         filter: "교내동아리",
         crewName: "들불",
         crewImg: deulBul,
-        join_date: "12월 6일"
+        join_date: "11월 15일 ~ 11월 30일",
+        joining: true
     },
     {
         filter: "교내동아리",
         crewName: "민족예술학회",
         crewImg: artCrew,
-        join_date: "12월 7일"
+        join_date: "11월 20일 ~ 12월 7일",
+        joining: false
     },
     {
         filter: "교내동아리",
         crewName: "왕산악",
         crewImg: wangSanAk,
-        join_date: "12월 5일"
+        join_date: "11월 21일 ~ 12월 5일",
+        joining: false
     },
     {
         filter: "교내동아리",
         crewName: "들불",
         crewImg: deulBul,
-        join_date: "12월 6일"
+        join_date: "11월 15일 ~ 11월 30일",
+        joining: true
     },
     {
         filter: "연합동아리",
         crewName: "DoIt",
         crewImg: doIt,
-        join_date: "11월 5일"
+        join_date: "11월 5일 ~ 11월 26일",
+        joining: true
     },
     {
         filter: "연합동아리",
         crewName: "미팅놈들",
         crewImg: meetingNom,
-        join_date: "11월 30일"
+        join_date: "11월 14일 ~ 11월 30일",
+        joining: false
     },
     {
         filter: "연합동아리",
         crewName: "DoIt",
         crewImg: doIt,
-        join_date: "11월 5일"
+        join_date: "11월 5일 ~ 11월 26일",
+        joining: true
     },
     {
         filter: "연합동아리",
         crewName: "미팅놈들",
         crewImg: meetingNom,
-        join_date: "11월 30일"
+        join_date: "11월 14일 ~ 11월 30일",
+        joining: false
     },
     {
         filter: "소모임",
         crewName: "곰돌이",
         crewImg: teddyBear,
-        join_date: "11월 30일"
+        join_date: "11월 10일 ~ 11월 30일",
+        joining: true
     },
     {
         filter: "소모임",
         crewName: "고양이",
         crewImg: cat,
-        join_date: "11월 2일"
+        join_date: "10월 20일 ~ 11월 2일",
+        joining: false
     },
     {
         filter: "소모임",
         crewName: "곰돌이",
         crewImg: teddyBear,
-        join_date: "11월 30일"
+        join_date: "11월 10일 ~ 11월 30일",
+        joining: true
     },
     {
         filter: "소모임",
         crewName: "고양이",
         crewImg: cat,
-        join_date: "11월 2일"
+        join_date: "10월 20일 ~ 11월 2일",
+        joining: false
     }
 ]
